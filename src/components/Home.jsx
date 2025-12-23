77a357c0dbf82962866d5303b7917766cf621352
techieshruti <shrutichandra721@gmail.com>
2025-12-23 17:14:07 +0530

    styling home componentr

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=''>
+        <h1 className='font-bold text-xl'>Welcome to PopX</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. B</p>
+        <button 
+        className='bg-purple-500 rounded-lg px-10 py-2'
+        >Create Account</button>
+        <button
+        className='bg-purple-300 rounded-lg px-10 py-2'
+        >Already Registered? Login</button>
+    </div>
+  )
+}
+
+export default Home
