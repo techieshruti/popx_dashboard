cb95b342632cf8e9e22ee714a9e751895b4e70d4
techieshruti <shrutichandra721@gmail.com>
2025-12-23 17:33:36 +0530

    styling home componentr

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex flex-col gap-2 items-start justify-center'>
-        <h1 className='font-bold text-xl'>Welcome to PopX</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. B</p>
-        <button 
-        className='bg-purple-500 rounded-lg px-10 py-2'
+    <div className='flex flex-col justify-end items-center h-full px-6 py-14'>
+        <div className=' w-65'>
+            <h1 className='font-bold text-xl'>Welcome to PopX</h1>
+        <p className='pb-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </div>
+        
+        <div className='flex flex-col gap-2'>
+            <button 
+        className='bg-[#6C25FF] text-white font-bold rounded-lg px-8 py-2'
         >Create Account</button>
         <button
-        className='bg-purple-300 rounded-lg px-10 py-2'
+        className='bg-[#CEBAFB] font-bold rounded-lg px-8 py-2 text-gray-800'
         >Already Registered? Login</button>
+        </div>
     </div>
   )
 }
