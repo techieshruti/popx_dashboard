aa7fbeab940d1cd4af3d3591808d0e679a1ccc9b
techieshruti <shrutichandra721@gmail.com>
2025-12-24 17:00:40 +0530

    styling home componentr

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='flex flex-col justify-end items-center h-full px-6 py-14 bg-gray-100'>
-        <div className=' w-65'>
-            <h1 className='font-bold text-xl'>Welcome to PopX</h1>
-        <p className='pb-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    <div className="flex justify-center items-center bg-gray-200 min-h-screen">
+      <div className="w-[375px] h-[812px] flex flex-col-2 justify-end px-6 pb-12">
+        <div className="mb-8">
+          <h1 className=" font-bold text-xl mb-2">Welcome to PopX</h1>
+          <p className="pb-6 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
         </div>
-        
-        <div className='flex flex-col gap-2'>
-            <button 
-        className='bg-[#6C25FF] text-white font-bold rounded-lg px-8 py-2'
-        >Create Account</button>
-        <button
-        className='bg-[#CEBAFB] font-bold rounded-lg px-8 py-2 text-gray-800'
-        >Already Registered? Login</button>
-        </div>
-    </div>
-  )
-}
 
-export default Home
+        <div className="">
+          <button className="bg-[#6C25FF] text-white font-bold rounded-lg px-8 py-2">
+            Create Account
+          </button>
+          <button className="bg-[#CEBAFB] font-bold rounded-lg px-8 py-2 text-gray-800">
+            Already Registered? Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
