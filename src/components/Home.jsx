6ffc7fc43380b1b311ed5c70b523c4826c0b5327
techieshruti <shrutichandra721@gmail.com>
2025-12-24 17:12:41 +0530

    styling home componentr

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center bg-gray-200 min-h-screen">
-      <div className="w-[375px] h-[812px] flex flex-col-2 justify-end px-6 pb-12">
-        <div className="mb-8">
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-[300px] h-[550px] flex flex-col pb-8 bg-gray-50 border border-gray-300 justify-end px-4">
+
+        {/* introduction */}
+        <div className="mb-2">
           <h1 className=" font-bold text-xl mb-2">Welcome to PopX</h1>
-          <p className="pb-6 text-gray-500">
+          <p className="pb-4 text-gray-500">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           </p>
         </div>
 
-        <div className="">
+        <div className="flex flex-col gap-2">
           <button className="bg-[#6C25FF] text-white font-bold rounded-lg px-8 py-2">
             Create Account
           </button>
