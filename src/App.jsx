3325074ebdcece311452f6e6b79ad6a797811fa0
techieshruti <shrutichandra721@gmail.com>
2025-12-25 15:19:50 +0530

    added account component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './components/Home'
 import SignIn from './components/SignIn'
+import Account from './components/Account'
 
 const App = () => {
   return (
     <div  className="flex justify-center items-center min-h-screen">
       {/* <Home/> */}
-      <SignIn/>
+      {/* <SignIn/> */}
+      <Account/>
     </div>
   )
 }
