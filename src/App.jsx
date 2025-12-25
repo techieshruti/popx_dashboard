f3d61886e3c40a218965915c99deb2f7cbf303a5
techieshruti <shrutichandra721@gmail.com>
2025-12-25 15:23:50 +0530

    created form in account component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Home from './components/Home'
 import SignIn from './components/SignIn'
 import Account from './components/Account'
 
+const formData=[
+  {id:1, name:"Sam", phone:123456789, email:"sam@gmail.com", password:"123", company:"company"}
+]
+
 const App = () => {
   return (
     <div  className="flex justify-center items-center min-h-screen">
--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div className='min-h-screen flex items-center justify-center'>
+        <div className='w-[375px] h-[812px] bg-gray-50 border border-gray-200 px-6 pt-10'>
+            <form action="">
+
+            </form>
+        </div>
+    </div>
   )
 }
 
