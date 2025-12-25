58b42b880c7e9e04d89a74c8c8308d2ec229d645
techieshruti <shrutichandra721@gmail.com>
2025-12-25 18:01:15 +0530

    added profile styles

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="w-[375px] h-[812px] pb-8 bg-gray-100 border border-gray-300 justify-end px-4">
-      <div className="w-[375px] h-[812px] bg-white border border-gray-200 flex flex-col">
+    <div className="w-[375px] h-[812px] pb-8 bg-gray-50 border border-gray-300 justify-end px-4">
+      <div className="w-[375px] h-[812px] bg-white flex flex-col">
         <div className="px-6 py-4 border-b border-gray-200">
           <h1 className="text-sm font-semibold text-gray-800">
             Account Settings
           </h1>
         </div>
 
-        {/* profile */}
+        <div className="h-[812px] bg-gray-50">
+            {/* profile */}
         <div className="px-6 py-5 flex items-center gap-4">
           {/* image */}
           <img
@@ -23,6 +24,7 @@ const Profile = () => {
             <p className="text-sm font-semibold text-gray-900">Marry Doe</p>
             <p className="text-xs text-gray-500">Marry@gmail.com</p>
           </div>
+        </div>
         </div>
       </div>
     </div>
