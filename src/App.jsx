bf10176ef313f4d73d827999512eedcf6e9d77a1
techieshruti <shrutichandra721@gmail.com>
2025-12-25 16:01:34 +0530

    styling form in account component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import SignIn from './components/SignIn'
 import Account from './components/Account'
 
 const formData=[
-  {id:1, label:"Full Name", placeholder:"Enter your name"},
-  {id:1, label:"Phone number", placeholder:"Enter phone number"},
-  {id:1, label:"Email address", placeholder:"Enter email"},
-  {id:1, label:"Password", placeholder:"Enter password"},
-  {id:1, label:"Company Name", placeholder:"Enter company name"},
+  {id:1, label:"Full Name", placeholder:"Enter your name", type: "text"},
+  {id:2, label:"Phone number", placeholder:"Enter phone number", type: "tel"},
+  {id:3, label:"Email address", placeholder:"Enter email",type: "email"},
+  {id:4, label:"Password", placeholder:"Enter password", type: "password"},
+  {id:5, label:"Company Name", placeholder:"Enter company name", type: "text"},
+  {id:6, label:"Company Name", placeholder:"Enter company name", type: "text"},
 ]
 
 const App = () => {
@@ -16,9 +17,7 @@ const App = () => {
     <div  className="flex justify-center items-center min-h-screen">
       {/* <Home/> */}
       {/* <SignIn/> */}
-      {formData.map((data,id)=>(
-        <Account key={id} data={formData}/>
-      ))}
+     <Account formData={formData}/>
       
     </div>
   )
--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Account = ({formData}) => {
+const Account = ({ formData }) => {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-        <div className='w-[375px] h-[812px] bg-gray-50 border border-gray-200 px-6 pt-10'>
-            <h1 
-            className='text-3xl font-bold mr-20 pb-4'
-            >Create your PopX account</h1>
-            <form action="">
-               <div className='relative'>
-              <label 
-            className='block absolute -top-2 left-3 bg-white px-1 text-xs text-[#6C25FF] mb-1'
-            htmlFor="password">{formData.label}</label>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-[375px] h-[812px] bg-gray-50 border border-gray-200 px-6 pt-10">
+        <h1 className="text-3xl font-bold mr-20 pb-4">
+          Create your PopX account
+        </h1>
+
+        <form>
+          {formData.map((data) => (
+            <div className="relative mb-6" key={data.id}>
+              <label
+                className="block absolute -top-2 left-3 bg-white px-1 text-xs text-[#6C25FF] font-semibold">
+                {data.label}
+              </label>
+              <input
+                className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none"
+                type={data.type}
+                placeholder={data.placeholder}
+              />
             </div>
-            <input 
-            className='w-full border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none'
-            type="text" placeholder={formData.placeholder} />
-
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="radio" />
-            </form>
-            <button 
-            className='w-full bg-purple-300 text-white font-semibold py-3 rounded-md cursor-pointer mt-2'>Create Account</button>
-        </div>
+          ))}
+        </form>
+        <button className="w-full bg-purple-300 text-white font-semibold py-3 rounded-md cursor-pointer mt-2">
+          Create Account
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
